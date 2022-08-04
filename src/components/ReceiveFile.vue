ed96dcd3939563e1dfb160d7a801d0a1a56b87e4
Abdul-Rahman <44008172+AbdulBarakeh@users.noreply.github.com>
2022-08-04 08:50:32 +0200

    generate guid

--- src/components/ReceiveFile.vue
+++ src/components/ReceiveFile.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
+import { Guid } from "guid-typescript";
 defineProps<{
   msg: string;
+  guid: Guid;
 }>();
+
+function setGuid(){
+  return Guid.create();
+}
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
+  <h3>To receive files provide the sender with the given id:</h3>
+  <br />
+  <p>{{ setGuid() }}</p>
 </template>
 
 <style scoped>
